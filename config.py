29065e7dcb4b090515039ee5e266d52ef510cd8f
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-03-25 15:02:31 +0100

    Add application identity configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+# ==========================================
+#        IDENTITÉ DE L'APPLICATION
+# ==========================================
+
+APP_NAME = "Grand Paname"
+APP_VERSION = "v1.1"
+APP_CODENAME = "Abondance 🧀" # Le petit nom de ta mise à jour
+APP_SUBTITLE = "Naviguez le Grand Paris, tout simplement."
