ffa63045c7c92c6022a29b1ca2c0e93b597da564
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-03-25 14:44:24 +0100

    Add API request functions for Streamlit app

---
+++ api_idfm.py
@@ -0,0 +1,21 @@
+import requests
+import streamlit as st
+from constants import API_KEY, BASE_URL
+
+def demander_api(suffixe):
+    headers = {'apiKey': API_KEY.strip()}
+    try:
+        r = requests.get(f"{BASE_URL}/{suffixe}", headers=headers)
+        return r.json()
+    except Exception as e: 
+        # On pourrait ajouter un st.error() ici plus tard pour mieux gérer les pannes !
+        return None
+
+@st.cache_data(ttl=3600)
+def demander_lignes_arret(stop_id):
+    headers = {'apiKey': API_KEY.strip()}
+    try:
+        r = requests.get(f"{BASE_URL}/stop_areas/{stop_id}/lines", headers=headers)
+        return r.json()
+    except: 
+        return None
