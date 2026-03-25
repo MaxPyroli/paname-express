3259e838fed158e5634ea28f635bde88d405380c
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-03-25 15:04:26 +0100

    Update application version to v1.1.2

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 # ==========================================
 
 APP_NAME = "Grand Paname"
-APP_VERSION = "v1.1"
+APP_VERSION = "v1.1.2"
 APP_CODENAME = "Abondance 🧀" # Le petit nom de ta mise à jour
 APP_SUBTITLE = "Naviguez le Grand Paris, tout simplement."
