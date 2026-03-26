1fb608ce0cfa2e6a765f0fd37f11a64d4ab5d682
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-03-26 18:01:53 +0100

    Update APP_VERSION to v2.0

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 # ==========================================
 
 APP_NAME = "Grand Paname"
-APP_VERSION = "v1.2BETA"
+APP_VERSION = "v2.0"
 APP_CODENAME = "Beaufort 🧀" # Le petit nom de ta mise à jour
 APP_SUBTITLE = "Naviguez le Grand Paris, tout simplement."
