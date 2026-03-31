0250385816b1f74e1f8da0610a0196a34700e0d2
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-03-31 09:36:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 # ==========================================
 
 APP_NAME = "Grand Paname"
-APP_VERSION = "v2.0.1"
+APP_VERSION = "v2.67"
 APP_CODENAME = "Beaufort 🧀"
 APP_SUBTITLE = "Naviguez le Grand Paris, tout simplement."
