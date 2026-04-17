2d70c8562079f729a7dfc9a6ae7fcf9d3c6af65d
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-04-17 16:18:29 +0200

    Update and rename config.py to settings.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# ==========================================
-#        IDENTITÉ DE L'APPLICATION
-# ==========================================
-
-APP_NAME = "Grand Paname"
-APP_VERSION = "v2.3"
-APP_CODENAME = "Beaufort 🧀"
-APP_SUBTITLE = "Naviguez le Grand Paris, tout simplement."
