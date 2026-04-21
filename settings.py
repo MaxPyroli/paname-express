ba4c0c7634ec3d25a2e614b4286f596fbe2a5194
MaxPyroli <56940272+MaxPyroli@users.noreply.github.com>
2026-04-21 18:57:08 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ import streamlit as st
 # ==========================================
 
 APP_NAME = "Grand Paname"
-APP_VERSION = "v2.3"
+APP_VERSION = "v2.3.1"
 APP_CODENAME = "Beaufort 🧀"
 APP_SUBTITLE = "Naviguez le Grand Paris, tout simplement."
 
